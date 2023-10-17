96599e29e1714040c8a86b62168cfd9fe38e2811
anaky94 <anaky.44@gmail.com>
2023-10-17 23:49:11 +0200

    epreuve eau 02

--- eau01.py
+++ eau01.py
@@ -1,9 +1,29 @@
 
 
+""" Créez un programme qui affiche toutes les différentes combinaisons de deux nombre entre 00 et 99 dans l’ordre croissant.
 
 
-for a in range(0, 10):
-    for b in range (0,10):
-        for c in range (0, 10):
-            if a != b and a != c and b != c:
-                print(a, b, c, end=',  ')
+Exemples d’utilisation :
+$> python exo.py
+00 01, 00 02, 00 03, 00 04, ... , 00 99, 01 02, ... , 97 99, 98 99
+$>
+"""
+# 1: commencer par afficher 2 nombre tel que : 00 01, 00 02
+# 2: ensuite afficher : 00 02  
+# 3: 
+# 4: 
+# 5: 
+
+a=0
+b=0
+c=0
+d=0
+
+for  a in range (0,10):
+    for b in range(0,10):
+        print(a,b, end="")
+
+
+
+
+
