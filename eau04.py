812aa2b564f393fd33823b3d7bbaef919a25fd28
anaky94 <anaky.44@gmail.com>
2023-10-25 00:07:31 +0200

    eau 04

--- eau04.py
+++ eau04.py
@@ -10,10 +10,23 @@ $>
 Afficher -1 si le paramètre est négatif ou mauvais.
 
 """
-a, b, c, = 0, 1 ,1
-while c <:
 
-    print(b ,end=",")
+"""
+Errors
+    renvoyer une erreur si plus d'un arguments 
+    renvoyer une erreur si l'arguments est une chaine de caractère 
+"""
+# 1 : enté : nombre utilisateur 
+    #nombre utilisateur =x
+# 2 : creer une liste vide 
+# si l'enté 
+a, b, c, = 0, 1 ,1
+
+fibo=input("entret un nombre : ")
+
+def suite_fibo:
+    while fibo < :
+    print(b, end=" " )
     a,b,c= b, a+b, c+1
 
 
