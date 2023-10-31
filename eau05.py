d4f8aa5c65471e099768aaa98bb182864730aa3e
anaky94 <anaky.44@gmail.com>
2023-10-31 10:22:36 +0100

    epreuve eau 05

--- eau05.py
+++ eau05.py
@@ -14,43 +14,25 @@ Afficher -1 si le paramètre est négatif ou mauvais.
 
 def is_prime(n):
     
-    if n % 2 ==0:
-        return False
-    for i in range (3, int(n** 0.5) + 1,2):
-        if n % i ==0:
-            return False
-    return True
+   if not isinstance(n, int):
+    return False
+  if n % 2 == 0:
+    return False
+  for i in range(3, int(math.sqrt(n)) + 1):
+    if n % i == 0:
+      return False
+  return True
 
 
 
-get_next_prime(n)
-"""Retourne le nombre premier supérieur ou égal à n.
+def get_next_prime(n):
+  i = n + 1
+  while True:
+    if is_prime(i)== False:
+      i +=1
+    else:
+      print(i)
+      return
+      
 
-  Args:
-    n: Le nombre à partir duquel on commence la recherche.
-
-  Returns:
-    Le nombre premier supérieur ou égal à n.
-  """
-    i = n + 1
-    while True:
-        if is_prime(i):
-            return i
-        i += 1
-
-def main():
-    """Affiche le nombre premier supérieur à n.
-
-  Args:
-    n: Le nombre à partir duquel on commence la recherche.
-
-  Returns:
-    Rien.
-  """
-
-n = int(input("Entrez un nombre: "))
-nex_prime=get_next_prime(n)
-print("Le nombre premier supérieur à ",n, "est", next_prime)
-
-
-main()
+print(get_next_prime(14))
