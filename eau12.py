3a8457618f483f9d493ffa30dba8eb8da59820f2
anaky94 <anaky.44@gmail.com>
2023-11-22 23:40:59 +0100

    epreuve eau 12

--- eau12.py
+++ eau12.py
@@ -14,7 +14,7 @@ $> python exo.py -8 -6 4
 # afficher le minimum entre 2 element d'un tableau constituer de nombre 
 # dans un tableau de nombre afficher le nombre minimum, 
 #  afficher en tableau de nombreen avec une variable table=["3", "4", "7", "9"]
-# par la boucle for parcourir le tableau et affichrle le nombre mini dans ce tableau 
+# par la boucle for parcourir le tableau et afficher le le nombre mini dans ce tableau 
 # afficher le minimum entre lindex 3 du tableau et l'index 2
      afficher le minimum
     # afficher  le minimum entre lindex 1 du tableau et l'index 0
@@ -22,9 +22,6 @@ $> python exo.py -8 -6 4
 afficher le minimum entre le resulta de l'index 3 et 2 et le result de l'index de l'index 1 et 0 
 
 """
-
-
-
 """
 def parcou_laboucle_for(): 
     i=["-13",  "2", "3", "19"]
@@ -32,13 +29,33 @@ def parcou_laboucle_for():
     for i in range (len(i)):
         print( min_value)
     return min_value
-
 print(parcou_laboucle_for())
-
 """
-i =      ["-13",  "2", "3", "19"]
+
 # indice:    0     1    2     3
+def trie():
+    i = ["-13", "3", "7", "19" ]
 
-min1 = i[-1] and i [-2]
+    for k in range(len(i) - i):
+        for j in range (k +1, len(i)):
+            if i[k] > i[j]:
+                i[k], i[j] = i[j], i[k]
+            
 
-print(i[-2])
+print(trie)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- test.py
+++ test.py
@@ -2,7 +2,7 @@
 
 
 
-
+import time
 
 
 """
@@ -40,16 +40,65 @@ phrase=" ".join(mots)
 
 print(phrase)
 """
+"""
+
+print("ce Programme vous permet de voir quel temps met un oeuf à cuire selon sa taille et sa température ")
+print("pour commencer, choisissez la taille de l'oeuf")
+print ("1 - Oeufs à la coque : 3 minutes")
+print("2 - Oeufs mollets : 6 minutes")
+print("3 - Oeufs durs : 9 minutes")
+choix= input("voustre choix (1 , 2 ou 3) : ")
+
+if choix =="1":
+    for i in range (10):
+        time.sleep(1)
+        print(".", end="", flush=True)
 
 
+if choix == "2":
+    for i in range (10):
+        time.sleep(1)
+        print(".", end="", flush=True)
+    time.sleep(1)
+    print("6 minute ",".", end="", flush=True)
+    print("temps restant", "5 minute", ".", end="", flush=True)
+    
+    
 
 
+if choix == "3":
+    time.sleep(1)
+    print(".", end="", flush=True)
+
+"""
+""" 
+ tablau trié 1 5 8 10 
+ trier le bableau 
+ trouver l'ecart enntre 1 et 5 en suite 5 et 8 et 8 et 10
+"""
+
+ma_liste= [4, 1, 19, 3, 67]
+
+def trier():
+   
+    ma_liste.sort()
+    print(ma_liste)
+
+trier() # resultat du tableau trier 
 
 
+# trouver l'ecart entre 1 et 5 en suite 5 et 8 et 8 et 10 
+
+def ecart():
+    for i in range(len(ma_liste)-1):
+        print(ma_liste[i+1]-ma_liste[i])
+    
+    
 
 
+""""
 
-
+"""
 
 
 
