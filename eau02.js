f9299ee1f7f816605f61553823b3936e0285e9ce
anaky94 <anaky.44@gmail.com>
2025-01-21 00:59:10 +0100

    lern eau

--- eau02.js
+++ eau02.js
@@ -6,40 +6,18 @@ $> python exo.py
 00 01, 00 02, 00 03, 00 04, ... , 00 99, 01 02, ... , 97 99, 98 99
 $>
 */
-// let a =0;
-// let b=0;
-// let c=0;
-// let d=0;
-// for(let a = 0; a < 10 ;){
-//     for(let b=0; b<10;){
-//         for(let c=0;c<10;){
-//             for(let d=0; d<10;){
-//                 console.log(a&b, b&c)
-//             }
+const generationCombinaisons =(min=0, max=99) => {
+    const combinanison = [];
 
-//         }
-//     }
-// }
-
-const  Combin = () => {
-    let result = "";
-    for (let i=0; i<=10; i++){
-        for (let j = i + 1; j <=10; j++){
-                result += `${i}${j},`;
-            
-            
-        }
-        
-    }
-    return result.slice  (0,-1);
-    
-    const errreuAvecArgument = () => {
-        if (process.argv[2]){
-            console.log("Erreur ; L'argument n'est pas nécessaire pour ce script")
-    
-        }else {
-            console.log(Combin())
-        }
-    };
 }
-console.log(Combin());
+
+// validation des paramètres 
+if (min<0 || max > 99 || min >= max){
+    console.log("Les paramètres doivent être entre 0 et 99, et min doit être inférieur à max")
+}
+
+//génération des combinaisons
+for (let i= min; i <= max; i++){
+    for (let j =i +1; j<max; j++)
+        combinations.push( `${}`)
+}git add 
