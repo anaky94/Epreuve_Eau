b222fe563feb9db7103ee08de5a84fdcad3fccb6
anaky94 <anaky.44@gmail.com>
2025-02-20 10:45:44 +0100

    eau03

--- eau03.js
+++ eau03.js
@@ -14,52 +14,65 @@ Afficher error et quitter le programme en cas de problèmes d’arguments.
 
  */
 //parsing
-const args = process.argv.slice(2);
-// function getArgument () {
-//     const args = process.argv.slice(2);
-//     return args;
-// }
 
-// // Gestion d'erreur 
-// function isValideArguments (args) {
-//     if (args.length >= 1) {
-//         return true;
-//     }else {
-//         console.error("ce script nécessite minimum 2 arguments");
-//         return false;
-
-//     }
-// }
-
-// résolution
-// function resolve (args) {
-//     const reverseArgs = [];
-//     for (let i = args.length - 1; i >= 0; i--){
-//         reverseArgs.push(args[i]);
-//     }
-//     return reverseArgs;
-// }
-
-const array = ["je ", "suis", "drole","et"]
-const Newarray =[];
-for( let i=array.length-1; i>=0; i-- ){
-    Newarray.push(array[i]);
-    console.log(Newarray.join(', '));
+function getArgument () {
+    const args = process.argv.slice(2);
+    return args;
 }
 
+// Gestion d'erreur 
+function isValideArguments (args) {
+    if (args.length >= 1) {
+        return true;
+    }else {
+        console.error("ce script nécessite minimum 2 arguments");
+        return false;
+
+    }
+}
+
+// résolution
+function resolve () {
+    const args = process.argv.slice(2);//recupération des arguments
+
+    if(!isValideArguments(args)){//vérification des arguments 
+        return;
+    }
+
+    const reverseArgs = [];
+    for (let i = args.length - 1; i >= 0; i--){
+        reverseArgs.push(args[i]);
+    }
+    return reverseArgs;
+}
 
 // resolve();
 
 //Afficher le resultat
-// function display() {
-//     const args = getArgument();
-//     if (!isValideArguments(args)){
-//         return;
+function display() {
+    const args = getArgument();
+    if (!isValideArguments(args)){
+        return;
+    }
+
+    const result = resolve();
+    if (result) {
+        console.log(result.join(" "));
+
+    }
+}
+display();
+
+
+
+// function secnAray (){
+//     const array = ["je ", "suis", "drole","et"]
+//     const Newarray =[];
+//     for( let i=array.length-1; i>=0; i-- ){
+//         Newarray.push(array[i]);
+//         console.log(Newarray.join(', '));
 //     }
 
-//     const result = resolve();
-//     console.log(result);
-
 // }
-// display();
 
+// secnAray(); 
